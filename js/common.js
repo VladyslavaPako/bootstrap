c83cb42773f3e1f4831d11f5a0b6e925d1001c9c
Vladyslava <32717207+VladyslavaPako@users.noreply.github.com>
2019-01-30 20:01:07 +0200

    fixes

--- js/common.js
+++ js/common.js
@@ -1,25 +1,4 @@
 $(document).ready(function(){
   $('.parallax').parallax();
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true
-  });
-  $(".dropdown-trigger").dropdown();  
-  $('.sidenav').sidenav(); 
-  $('.owl-carousel').owlCarousel({
-    stagePadding: 50,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+ 
 });
