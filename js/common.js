62756b941fabd2a205616255e429989506990d22
Vladyslava <32717207+VladyslavaPako@users.noreply.github.com>
2019-01-30 18:45:45 +0200

    0.14
    Made carousel
    Fixed little parts
    Try to made video (doesn't work)

--- js/common.js
+++ js/common.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    $('.parallax').parallax();
+  $('.parallax').parallax();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
   });
+  $(".dropdown-trigger").dropdown();  
+  $('.sidenav').sidenav(); 
+  $('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+});
