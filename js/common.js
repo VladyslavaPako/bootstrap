3a56c7092c279733b5d604b88d8ded92f656ec30
Vladyslava <32717207+VladyslavaPako@users.noreply.github.com>
2019-01-31 13:03:12 +0200

    0.18
    And carousel

--- js/common.js
+++ js/common.js
@@ -19,23 +19,6 @@ $(document).ready(function(){
 })
 
 
-$('.popa-list').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },  
-      600:{ 
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
-})
-
 
 
   
